732ef9a46603da44d9fa7520486f3bf358588eb1
Anatolii Tupkalo <anatoliitupkalo@Macs-iMac.local>
2020-02-12 13:12:57 -0700

    page titles were updated

--- source/js/js.sections/services.js
+++ source/js/js.sections/services.js
@@ -117,3 +117,27 @@ window.onload = function(){
     });
 
 };
+
+//------------------------------------------------------      tabs
+
+const titleTabs = document.querySelectorAll('.services__tabs-item');
+const contTabs = document.querySelectorAll('.services__tabs-cont-item');
+
+for(let i = 0; i < titleTabs.length; i++){
+
+    titleTabs[i].addEventListener('click', function(){
+        const $this = this;
+        for(let i = 0; i < titleTabs.length; i++){
+            titleTabs[i].classList.remove('tabs__title-active');
+            contTabs[i].classList.remove('content__item-active');
+        };
+        function addClass(){
+            $this.classList.add('tabs__title-active');
+            contTabs[i].classList.add('content__item-active');
+        };
+        setTimeout(function(){
+            addClass();
+        }, 200);
+
+    });
+}
