7410dde36f3e4fd02b6e6460e58b2a1b67a8ebc6
Anatolii Tupkalo <anatoliitupkalo@Macs-iMac.local>
2020-10-27 15:44:36 -0600

    finale commit 3.5

--- source/js/js.sections/obits.js
+++ source/js/js.sections/obits.js
@@ -1,3 +1,111 @@
+const obitsPageData = [
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    },
+    {
+        picLink: '/assets/img/obits-test.jpg',
+        fName: 'firstName',
+        lName: 'lastName',
+        pageLink: '/obits-single.html'
+    }
+];
+
+const obitPic = document.querySelectorAll('.obits__list-img');
+const f_Name = document.querySelectorAll('.obits__list-firstname');
+const l_Name = document.querySelectorAll('.obits__list-lastname');
+const p_Link = document.querySelectorAll('.obits__list-link');
+
+
+
+    for(let i = 0; i < obitsPageData.length; i++){
+        setElements(i);
+    };
+
+
+
+function setElements(a){
+    obitPic[a].setAttribute('src', obitsPageData[a].picLink );
+    f_Name[a].innerHTML = obitsPageData[a].fName;
+    l_Name[a].innerHTML = obitsPageData[a].lName;
+    p_Link[a].setAttribute('href', obitsPageData[a].pageLink);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const pageTitle = document.querySelector('.obits__title');
 window.addEventListener('scroll', function(){
